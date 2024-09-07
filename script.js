1548847ef2dbef203538f549718650975ea97b3a
JackM88 <jackmoree88@gmail.com>
2024-09-06 21:28:50 -0400

    Menu creation

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const a = 1
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+    // centeredSlides: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+
+
+  
